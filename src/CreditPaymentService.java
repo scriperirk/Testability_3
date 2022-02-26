e7218ab525ce9f8459e6999e621bd3526f907edd
Scriper.irk <scriper.irk@gmail.com>
2022-02-27 01:23:44 +0800

    initial commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,9 @@
 public class CreditPaymentService {
-    public double calculate(int credit, double procent, double term){
+    public double calculate(int credit, double procent, double term) {
 
-        double i = Math.round((procent/100/12)*1000000)/1000000.0d;
-        //double degree = Math.pow((1+i),term)-1;
-        double degree = Math.round((Math.pow((1+i),term)-1)*1000000)/1000000.0d;
-        double payments = credit * (i + (i/degree));
+        double i = Math.round((procent / 100 / 12) * 1000000) / 1000000.0d;
+        double degree = Math.round((Math.pow((1 + i), term) - 1) * 1000000) / 1000000.0d;
+        double payments = credit * (i + (i / degree));
 
         return payments;
     }
--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
 
         //год
         double payments1 = service.calculate(1_000_000, 9.9, 12);
-        System.out.println("Ежемесячный платеж: " +(int) payments1);
+        System.out.println("Ежемесячный платеж: " + (int) payments1);
 
         //два
         double payments2 = service.calculate(1_000_000, 9.9, 24);
         System.out.println("Ежемесячный платеж: " + (int) payments2);
 
         //три
-        double payments3 = service.calculate(1_000_000, 9.9,36);
+        double payments3 = service.calculate(1_000_000, 9.9, 36);
         System.out.println("Ежемесячный платеж: " + (int) payments3);
     }
 }
